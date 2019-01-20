58e998385328d4c5e443aabb961169cc174b95b2
Ayanrocks <ayanb1999@gmail.com>
2019-01-20 22:03:06 +0530

    added try eccept to memory parameters

--- cli/cli_modules/memory.py
+++ cli/cli_modules/memory.py
@@ -22,19 +22,26 @@ def display_details(mem, swap_mem):
     print("\033[93mUsed Memory:\033[0m " + str(mem[2]) + "%")
     print("\033[93mUsed Memory:\033[0m " +
           str(round(total_memory - available_memory, 2)) + " GB")
-    print("\033[93mBuffers:\033[0m " +
-          str(round(mem[7] / 1024 / 1024 / 1024, 2)) + " GB")
-    print("\033[93mCached:\033[0m " +
-          str(round(mem[8] / 1024 / 1024 / 1024, 2)) + " GB")
-    print("\033[93mShared:\033[0m " +
-          str(round(mem[9] / 1024 / 1024 / 1024, 2)) + " GB")
-    print("\n\033[91mSWAP MEMORY:\033[0m \n ")
-    print("\033[93mTotal Swap Memory:\033[0m " +
-          str(round(total_swap_memory, 2)) + " GB")
-    print("\033[93mUsed Swap Memory:\033[0m " +
-          str(round(swap_mem[2] / 1024 / 1024 / 1024, 2)) + " GB")
-    print("\033[93mAvailable Swap Memory:\033[0m " +
-          str(round(swap_mem[3] / 1024 / 1024 / 1024, 2)) + " GB")
+    try:
+        print("\033[93mBuffers:\033[0m " +
+              str(round(mem[7] / 1024 / 1024 / 1024, 2)) + " GB")
+        print("\033[93mCached:\033[0m " +
+              str(round(mem[8] / 1024 / 1024 / 1024, 2)) + " GB")
+        print("\033[93mShared:\033[0m " +
+              str(round(mem[9] / 1024 / 1024 / 1024, 2)) + " GB")
+
+    except:
+        pass
+
+    finally:
+
+        print("\n\033[91mSWAP MEMORY:\033[0m \n ")
+        print("\033[93mTotal Swap Memory:\033[0m " +
+              str(round(total_swap_memory, 2)) + " GB")
+        print("\033[93mUsed Swap Memory:\033[0m " +
+              str(round(swap_mem[2] / 1024 / 1024 / 1024, 2)) + " GB")
+        print("\033[93mAvailable Swap Memory:\033[0m " +
+              str(round(swap_mem[3] / 1024 / 1024 / 1024, 2)) + " GB")
 
 
 def memory_info():
